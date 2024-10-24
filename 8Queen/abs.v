96343c369883e66370aa2906c89d708962d82fae
Moein <moein.akbari2003@gmail.com>
2024-10-24 12:32:30 +0330

    checkpoint

---
+++ 8Queen/abs.v
@@ -0,0 +1,10 @@
+module abs (
+    value,
+    number
+);
+    parameter N = 3;
+    input [N:1] number;
+    output [N:1] value;
+
+    assign value = number >= 0 ? number : -number
+endmodule
--- 8Queen/is_safe.v
+++ 8Queen/is_safe.v
@@ -4,6 +4,23 @@ module is_safe (
 );
     input [2:0] row1, column1, row2, column2;
     output safe;
+    wire [2:0] row_difference,
+               column_difference, 
+               absolute_row_difference,
+               absolute_column_difference;
+    wire same_row, same_column, diagonal;
+
+    assign row_difference = row1 - row2;
+    assign column_difference = column1 - column2;
     
-    assign safe = ~(row1 == row2 || column1 == column2 || 
+    abs absolute_row_3bit #3 
+        (.value(absolute_row_difference), .number(row_difference));
+    abs absolute_column_3bit #3 
+        (.value(absolute_column_difference), .number(column_difference));
+    
+    assign same_row = (row1 == row2);
+    assign same_column = (column1 == column2);
+    assign diagonal = (absolute_row_difference == absolute_column_difference);
+    
+    assign safe = ~(same_row || same_column || diagonal)
 endmodule
