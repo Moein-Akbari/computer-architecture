8b23f91d5bfd3ec3f58a02653a0b32006077a912
Moein <moein.akbari2003@gmail.com>
2024-11-03 16:29:57 +0330

    done

--- hws/2/HW2_2_readable.s
+++ hws/2/HW2_2_readable.s
@@ -1,48 +1,52 @@
-addi sp, sp, -80
+# Init array
+    addi sp, sp, -80
 
-li t0, 20
-sw t0,  0(sp)
-li t0, 19
-sw t0,  4(sp)
-li t0, 18
-sw t0,  8(sp)
-li t0, 17
-sw t0,  12(sp)
-li t0, 16
-sw t0, 16(sp)
-li t0, 15
-sw t0, 20(sp)
-li t0, 14
-sw t0,  24(sp)
-li t0, 13
-sw t0, 28(sp)
-li t0, 12
-sw t0, 32(sp)
-li t0, 11
-sw t0, 36(sp)
-li t0, 10
-sw t0,  40(sp)
-li t0, 9
-sw t0, 44(sp)
-li t0, 8
-sw t0, 48(sp)
-li t0, 7
-sw t0, 52(sp)
-li t0, 6
-sw t0, 56(sp)
-li t0, 5
-sw t0, 60(sp)
-li t0, 4
-sw t0, 64(sp)
-li t0, 3
-sw t0, 68(sp)
-li t0, 2
-sw t0, 72(sp)
-li t0, 1
-sw t0, 76(sp)
+    li t0, 20
+    sw t0, 0(sp)
+    li t0, 19
+    sw t0, 4(sp)
+    li t0, 18
+    sw t0, 8(sp)
+    li t0, 17
+    sw t0, 12(sp)
+    li t0, 16
+    sw t0, 16(sp)
+    li t0, 15
+    sw t0, 20(sp)
+    li t0, 14
+    sw t0, 24(sp)
+    li t0, 13
+    sw t0, 28(sp)
+    li t0, 12
+    sw t0, 32(sp)
+    li t0, 11
+    sw t0, 36(sp)
+    li t0, 10
+    sw t0, 40(sp)
+    li t0, 9
+    sw t0, 44(sp)
+    li t0, 8
+    sw t0, 48(sp)
+    li t0, 7
+    sw t0, 52(sp)
+    li t0, 6
+    sw t0, 56(sp)
+    li t0, 5
+    sw t0, 60(sp)
+    li t0, 4
+    sw t0, 64(sp)
+    li t0, 3
+    sw t0, 68(sp)
+    li t0, 2
+    sw t0, 72(sp)
+    li t0, 1
+    sw t0, 76(sp)
 
+
+# Saving start address of array in s0
 add s0, sp, zero
 
+# loop1 iteration variable (i)
 addi s1, zero, 0 
 loop1:
     slti t0, s1, 76
@@ -62,13 +66,14 @@ loop1:
         
         bge t1, t0, end_if
         # swap
-        sw t1, 0(s1)
+        sw t1, 0(t3) # TODO: Check from the notes
         sw t0, 4(t3)
         end_if:
             addi s2, s2, 4 
             j loop2
     end_loop2:
-    addi s1, s1, 4 
-    j loop1
+        addi s1, s1, 4 
+        j loop1
 end_loop1:
-
+    # This line is just for putting breakpoint.
+    addi s11, zero, 0x7FFFFFA0
